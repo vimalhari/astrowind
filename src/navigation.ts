a1faeb1ef0c084f7dae3ed16a76efb420396d248
vimalhari <vimal_hari@hotmail.com>
2024-12-29 12:33:28 +0000

    navigation and about us page updated.

--- src/navigation.ts
+++ src/navigation.ts
@@ -25,47 +25,63 @@ export const headerData = {
       ],
     },
     {
-      text: 'Design',
+      text: 'Services',
       links: [
-        { text: 'Web Design', href: getPermalink('/web-design') },
-        { text: 'UX Design', href: getPermalink('/ux-design') },
+        { text: 'Website Development', href: getPermalink('/website-development') },
         { text: 'Graphic Design', href: getPermalink('/graphic-design') },
         { text: 'Video Production', href: getPermalink('/video-production') },
-      ],
-    },
-    {
-      text: 'Development',
-      links: [
-        { text: 'Web Development', href: getPermalink('/web-development') },
-        { text: 'Mobile App Development', href: getPermalink('/mobile-app') },
-        { text: 'Software Development', href: getPermalink('/software-development') },
-        { text: 'Ecommerce Solutions', href: getPermalink('/ecommerce-solutions') },
-        { text: 'Hugo Static Site', href: getPermalink('/hugo-static-site') },
-        { text: 'Astro Site', href: getPermalink('/astro-site') },
-        { text: 'WordPress', href: getPermalink('/wordpress') }, 
+
+        { text: 'Digital Marketing', href: getPermalink('/digital-marketing') },
+        { text: 'Social Media Marketing', href: getPermalink('/social-media-marketing') },
+
+        { text: 'IT Solutions', href: getPermalink('/it-solutions') },
+        { text: 'Cloud Services', href: getPermalink('/cloud-services') },
         { text: 'Automation', href: getPermalink('/automation') },
-        { text: 'ETL / Data Processing', href: getPermalink('/etl-data-processing') },
+        { text: 'ETL / Data Processing', href: getPermalink('/etl-data-processing') }, 
       ],
     },
-    {
-      text: 'Digital Marketing',
-      links: [
-        { text: 'SEO', href: getPermalink('/seo') },
-        { text: 'Social Media Marketing', href: getPermalink('/social-media-marketing') }, 
-        { text: 'Content Marketing', href: getPermalink('/content-marketing') }, 
-      ],
-    },
-    {
-      text: 'Technical',
-    links: [
-      { text: 'IT Support', href: getPermalink('/it-support') },
-      { text: 'IT Solutions', href: getPermalink('/it-solutions') },
-      { text: 'Cloud Services', href: getPermalink('/cloud-services') },
-      { text: 'Cybersecurity', href: getPermalink('/cyber-security') },
-      { text: 'Cloud Infrastructure', href: getPermalink('/cloud-infrastructure') },
-      { text: 'Office 365', href: getPermalink('/office-365') },
-    ],
-    },
+    // {
+    //   text: 'Design',
+    //   links: [
+    //     { text: 'Web Design', href: getPermalink('/web-design') },
+    //     { text: 'UX Design', href: getPermalink('/ux-design') },
+    //     { text: 'Graphic Design', href: getPermalink('/graphic-design') },
+    //     { text: 'Video Production', href: getPermalink('/video-production') },
+    //   ],
+    // },
+    // {
+    //   text: 'Development',
+    //   links: [
+    //     { text: 'Web Development', href: getPermalink('/web-development') },
+    //     { text: 'Mobile App Development', href: getPermalink('/mobile-app') },
+    //     { text: 'Software Development', href: getPermalink('/software-development') },
+    //     { text: 'Ecommerce Solutions', href: getPermalink('/ecommerce-solutions') },
+    //     { text: 'Hugo Static Site', href: getPermalink('/hugo-static-site') },
+    //     { text: 'Astro Site', href: getPermalink('/astro-site') },
+    //     { text: 'WordPress', href: getPermalink('/wordpress') }, 
+    //     { text: 'Automation', href: getPermalink('/automation') },
+    //     { text: 'ETL / Data Processing', href: getPermalink('/etl-data-processing') },
+    //   ],
+    // },
+    // {
+    //   text: 'Digital Marketing',
+    //   links: [
+    //     { text: 'SEO', href: getPermalink('/seo') },
+    //     { text: 'Social Media Marketing', href: getPermalink('/social-media-marketing') }, 
+    //     { text: 'Content Marketing', href: getPermalink('/content-marketing') }, 
+    //   ],
+    // },
+    // {
+    //   text: 'Technical',
+    // links: [
+    //   { text: 'IT Support', href: getPermalink('/it-support') },
+    //   { text: 'IT Solutions', href: getPermalink('/it-solutions') },
+    //   { text: 'Cloud Services', href: getPermalink('/cloud-services') },
+    //   { text: 'Cybersecurity', href: getPermalink('/cyber-security') },
+    //   { text: 'Cloud Infrastructure', href: getPermalink('/cloud-infrastructure') },
+    //   { text: 'Office 365', href: getPermalink('/office-365') },
+    // ],
+    // },
     {
       text: 'Blog',
       href: getBlogPermalink(),
@@ -76,44 +92,44 @@ export const headerData = {
 
 export const footerData = {
   links: [
-    {
-      title: 'Product',
-      links: [
-        { text: 'Features', href: '#' },
-        { text: 'Security', href: '#' },
-        { text: 'Team', href: '#' },
-        // { text: 'Enterprise', href: '#' },
-        { text: 'Customer stories', href: '#' },
-        { text: 'Pricing', href: '#' },
-        { text: 'Resources', href: '#' },
-      ],
-    },
-    {
-      title: 'Platform',
-      links: [
-        { text: 'Developer API', href: '#' },
-        { text: 'Partners', href: '#' },
-        { text: 'Atom', href: '#' },
-        { text: 'Electron', href: '#' },
-        { text: 'AstroWind Desktop', href: '#' },
-      ],
-    },
-    {
-      title: 'Support',
-      links: [
-        { text: 'Docs', href: '#' },
-        { text: 'Community Forum', href: '#' },
-        { text: 'Professional Services', href: '#' },
-        { text: 'Skills', href: '#' },
-        { text: 'Status', href: '#' },
-      ],
-    },
+    // {
+    //   title: 'Product',
+    //   links: [
+    //     { text: 'Features', href: '#' },
+    //     { text: 'Security', href: '#' },
+    //     { text: 'Team', href: '#' },
+    //     { text: 'Enterprise', href: '#' },
+    //     { text: 'Customer stories', href: '#' },
+    //     { text: 'Pricing', href: '#' },
+    //     { text: 'Resources', href: '#' },
+    //   ],
+    // },
+    // {
+    //   title: 'Platform',
+    //   links: [
+    //     { text: 'Developer API', href: '#' },
+    //     { text: 'Partners', href: '#' },
+    //     { text: 'Atom', href: '#' },
+    //     { text: 'Electron', href: '#' },
+    //     { text: 'AstroWind Desktop', href: '#' },
+    //   ],
+    // },
+    // {
+    //   title: 'Support',
+    //   links: [
+    //     { text: 'Docs', href: '#' },
+    //     { text: 'Community Forum', href: '#' },
+    //     { text: 'Professional Services', href: '#' },
+    //     { text: 'Skills', href: '#' },
+    //     { text: 'Status', href: '#' },
+    //   ],
+    // },
     {
       title: 'Company',
       links: [
         { text: 'About', href: '#' },
         { text: 'Blog', href: '#' },
-        { text: 'Careers', href: '#' },
+        // { text: 'Careers', href: '#' },
         // { text: 'Press', href: '#' },
         // { text: 'Inclusion', href: '#' },
         // { text: 'Social Impact', href: '#' },
@@ -132,7 +148,7 @@ export const footerData = {
     { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
     // { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/onwidget/astrowind' },
   ],
-  footNote: `© 2024 <a> Crizaze Business Services Ltd </a>. All rights reserved. <br>Registered in England & Wales. Company No. <a class="text-blue-600 underline dark:text-muted" href="https://find-and-update.company-information.service.gov.uk/company/15494238"> 15494238. </a>`,
+  footNote: `© 2024 <a> Crizaze Business Services Ltd. </a>All rights reserved. <br>Registered in England & Wales. Company No. <a class="text-blue-600 underline dark:text-muted" href="https://find-and-update.company-information.service.gov.uk/company/15494238"> 15494238. </a>`,
 };
 
 // class="text-blue-600 underline dark:text-muted" href="https://criztec.com/"
